0b4a2e94557a6b362afc9f3c7e0eed9004db28c2
MATVEN <mattiavena2993@gmail.com>
2019-11-06 00:31:59 +0100

    add hamburger menu js

---
+++ main.js
@@ -0,0 +1,9 @@
+// click per mostrare il menù
+$(".fas.fa-bars").click(function() {
+  $(".hamburger-menu").show();
+  });
+
+// click per nascondere il menù
+$(".fas.fa-times").click(function() {
+  $(".hamburger-menu").hide();
+  });
